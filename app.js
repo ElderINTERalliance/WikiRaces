e5aba3e9bd2a5500e6e852cd1ef2a710e2ec778d
zsarge <zackarywsargent@gmail.com>
2021-01-06 10:51:28 -0500

    add template for future development

---
+++ app.js
@@ -0,0 +1,12 @@
+var express = require('express');
+var app = express();
+var path = require('path');
+
+app.get('/', function(req, res){
+   // res.send("This is a test");
+   res.sendFile(path.join(__dirname + '/index.html'));
+});
+
+app.listen(3000);
+
+console.log("Started");
