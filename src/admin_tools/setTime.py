bd9630612140054b2ccd4a7f0510fa543d2222b5
zsarge <zackarywsargent@gmail.com>
2021-01-17 16:37:07 -0500

    add fancy text

--- src/admin_tools/setTime.py
+++ src/admin_tools/setTime.py
@@ -8,6 +8,19 @@ epoch = datetime.datetime.utcfromtimestamp(0)
 
 cursor = "> "
 
+print("")
+print("")
+print("oooooo   oooooo     oooo  o8o  oooo         o8o       ooooooooo.                                          ")
+print(" `888.    `888.     .8'   `\"'  `888         `\"'       `888   `Y88.                                        ")
+print("  `888.   .8888.   .8'   oooo   888  oooo  oooo        888   .d88'  .oooo.    .ooooo.   .ooooo.   .oooo.o ")
+print("   `888  .8'`888. .8'    `888   888 .8P'   `888        888ooo88P'  `P  )88b  d88' `\"Y8 d88' `88b d88(\"8 ")
+print("    `888.8'  `888.8'      888   888888.     888        888`88b.     .oP\"888  888       888ooo888 `\"Y88b.  ")
+print("     `888'    `888'       888   888 `88b.   888        888  `88b.  d8(  888  888   .o8 888    .o o.  )88b ")
+print("      `8'      `8'       o888o o888o o888o o888o      o888o  o888o `Y888\"\"8o `Y8bod8P' `Y8bod8P' 8\"\"888P' ")
+print("")
+print("This script is for setting when levels open.")
+print("")
+
 
 # CONVERT MINUTES FROM NOW INTO DATE STRING
 def minutes_to_date(mins: int) -> str:
