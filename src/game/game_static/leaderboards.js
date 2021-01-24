1f1260e3322658d56634b76579a6806efdfbdca8
zsarge <zackarywsargent@gmail.com>
2021-01-24 10:55:55 -0500

    add table template

--- src/game/game_static/leaderboards.js
+++ src/game/game_static/leaderboards.js
@@ -1,1 +1,7 @@
-alert("loaded");
+/*
+ * This file renders the information from the leaderboards api.
+ */
+
+document.getElementById("go-home").addEventListener("click", () => {
+	window.location.href = `${window.location.protocol}//${window.location.host}`;
+});
