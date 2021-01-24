84d606ca7f63bff4f862230756c410d359d803bb
zsarge <zackarywsargent@gmail.com>
2021-01-23 19:15:40 -0500

    calculate user times

--- src/game/leaderboards.js
+++ src/game/leaderboards.js
@@ -13,15 +13,68 @@ async function getLevelNames() {
 	return Object.keys(JSON.parse(file));
 }
 
+// gets difference in milliseconds
+async function getDateDeltas(open, close) {
+	const date = new Date(Date.parse(close) - Date.parse(open));
+	return date.getTime();
+}
+
+// return level time in miliseconds
+async function parseTime(obj) {
+	submission = await obj;
+	if (submission.levelOpen && submission.submitTime) {
+		return await getDateDeltas(submission.levelOpen, submission.submitTime);
+	}
+	return undefined; // replace with time level was open
+}
+
 // Should return object containing:
 // username
 // userid
 // sum time
 // level times
-async function getUserTimes(userId) {}
+async function getUserTimes(userId) {
+	const submissions = await db.getCollection(
+		{ userId: userId },
+		"submissions"
+	);
+	times = {};
+	totalTime = 0;
+	for (submission of submissions) {
+		time = await parseTime(submission);
+		times[submission.levelName] = time;
+		totalTime += Number(time);
+	}
+	return [totalTime, times];
+}
+
+async function getUserIds() {
+	const users = await db.getUsers();
+	let userIds = [];
+	for (user of users) {
+		userIds.push(user.userId);
+	}
+	return userIds;
+}
 
 async function getLeaderboards() {
-	return db.getUserById("1d8f2b5bfa19f0ef3626fe360a81a208f0dda67a");
+	const userIds = await getUserIds();
+
+	let leaderboard = [];
+
+	for (id of userIds) {
+		const user = await db.getUserById(id);
+		const [totalTime, times] = await getUserTimes(id);
+		leaderboard.push({
+			userId: id,
+			name: user.name,
+			totalTime: totalTime,
+			times: times,
+		});
+	}
+
+	// return leaderboard;
+	return leaderboard;
 }
 
 async function getLevelsLeaderboards() {
