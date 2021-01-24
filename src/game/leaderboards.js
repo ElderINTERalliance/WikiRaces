53bc31f76936b4a07efdda49507c971f2cbce727
zsarge <zackarywsargent@gmail.com>
2021-01-23 19:32:44 -0500

    dedupliate submission times

--- src/game/leaderboards.js
+++ src/game/leaderboards.js
@@ -8,9 +8,14 @@ const fs = require("fs").promises;
 const path = require("path");
 
 const staticFolder = path.join(__dirname, "/game_static/levels.json");
-async function getLevelNames() {
+
+async function getLevelData() {
 	const file = await fs.readFile(`${staticFolder}`, "utf-8");
-	return Object.keys(JSON.parse(file));
+	return JSON.parse(file);
+}
+
+async function getLevelNames() {
+	return Object.keys(await getLevelData());
 }
 
 // gets difference in milliseconds
@@ -19,20 +24,22 @@ async function getDateDeltas(open, close) {
 	return date.getTime();
 }
 
-// return level time in miliseconds
+async function getLevelDuration(levelName) {
+	const data = await getLevelData();
+	console.log("level:", getLevelDuration);
+	console.log(data);
+}
+
+// return level time in milliseconds
 async function parseTime(obj) {
 	submission = await obj;
+	console.log(submission);
 	if (submission.levelOpen && submission.submitTime) {
 		return await getDateDeltas(submission.levelOpen, submission.submitTime);
 	}
-	return undefined; // replace with time level was open
+	return getLevelDuration(submission.levelName);
 }
 
-// Should return object containing:
-// username
-// userid
-// sum time
-// level times
 async function getUserTimes(userId) {
 	const submissions = await db.getCollection(
 		{ userId: userId },
@@ -42,8 +49,18 @@ async function getUserTimes(userId) {
 	totalTime = 0;
 	for (submission of submissions) {
 		time = await parseTime(submission);
-		times[submission.levelName] = time;
-		totalTime += Number(time);
+		// If there are multiple submissions for one level, choose the shortest one
+		if (times[submission.levelName] !== undefined) {
+			if (times[submission.levelName] > time) {
+				totalTime -= Number(times[submission.levelName]);
+				times[submission.levelName] = time;
+				totalTime += Number(time);
+				console.log("duplicate deleted");
+			}
+		} else {
+			times[submission.levelName] = time;
+			totalTime += Number(time);
+		}
 	}
 	return [totalTime, times];
 }
@@ -73,7 +90,6 @@ async function getLeaderboards() {
 		});
 	}
 
-	// return leaderboard;
 	return leaderboard;
 }
 
