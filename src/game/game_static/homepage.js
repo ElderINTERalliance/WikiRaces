4d9ecfc2c47594bca3506f31cf216fc88c47f3b0
zsarge <zackarywsargent@gmail.com>
2021-01-31 09:47:19 -0500

    alight table headings left

--- src/game/game_static/homepage.js
+++ src/game/game_static/homepage.js
@@ -35,7 +35,7 @@ function createTableHeading() {
 
 	let time = document.createElement("th");
 	time.textContent = "Starts in:";
-	time.className = "align-right";
+	time.className = "align-left";
 
 	element.appendChild(numbers);
 	element.appendChild(links);
