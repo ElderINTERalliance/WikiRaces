623d09063280ee2d9b94ee31f4fa7e8a7d2ac9fb
zsarge <zackarywsargent@gmail.com>
2021-02-07 18:45:12 -0500

    load css properly

--- src/game/game_static/userSubmissions.js
+++ src/game/game_static/userSubmissions.js
@@ -1,1 +1,10 @@
+/* This script is loaded when a user Id is requested */
+document.getElementById("go-home").addEventListener("click", () => {
+	window.location.href = `${window.location.protocol}//${window.location.host}`;
+});
+
+document.getElementById("go-leaderboard").addEventListener("click", () => {
+	window.location.href = `${window.location.protocol}//${window.location.host}/wiki-races/leaderboard`;
+});
+
 alert("script loaded!");
