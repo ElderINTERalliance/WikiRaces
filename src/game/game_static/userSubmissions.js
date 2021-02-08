d0dd412b28727329a78f8912fdb70a0fefec71a8
zsarge <zackarywsargent@gmail.com>
2021-02-07 19:15:30 -0500

    display username on page load

--- src/game/game_static/userSubmissions.js
+++ src/game/game_static/userSubmissions.js
@@ -6,3 +6,43 @@ document.getElementById("go-home").addEventListener("click", () => {
 document.getElementById("go-leaderboard").addEventListener("click", () => {
 	window.location.href = `${window.location.protocol}//${window.location.host}/wiki-races/leaderboard`;
 });
+
+function generateURL(path) {
+	return `${window.location.protocol}//${window.location.host}${path}`;
+}
+
+function parseUserIdFromString(str) {
+	const lastSlash = str.lastIndexOf("/") + 1;
+	return str.slice(lastSlash);
+}
+
+function getIdFromUrl() {
+	const url = window.location.pathname;
+	return parseUserIdFromString(url);
+}
+
+async function getTextFrom(url) {
+	var resp;
+	var xmlHttp;
+	resp = "";
+	xmlHttp = new XMLHttpRequest();
+	if (xmlHttp != null) {
+		xmlHttp.open("GET", url, false);
+		xmlHttp.send(null);
+		resp = xmlHttp.responseText;
+	}
+	return resp;
+}
+
+async function getUserData() {
+	const pageUrl = window.location.pathname;
+	const apiUrl = pageUrl.replace("/leaderboard", "/api/leaderboard");
+	const resp = await getTextFrom(apiUrl);
+	return JSON.parse(resp);
+}
+
+(async () => {
+	userData = await getUserData();
+	const nameField = document.getElementById("display-name");
+	nameField.textContent = `Name: ${userData.name}`;
+})();
