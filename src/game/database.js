04d12be22815522c4eaea853a1e9542b8b4d14f4
zsarge <zackarywsargent@gmail.com>
2021-02-12 08:51:21 -0500

    remove outdated comment

--- src/game/database.js
+++ src/game/database.js
@@ -1,11 +1,4 @@
-/* This file contains the class that I use to act as an in memory database.
-   Originally, I wanted to use mongodb, because my friends had given it good reviews.
-   However, I don't really need a database to persist data after reboots, so I
-
-   app is small enough that it isn't really worth setting up a full database,
-   as all I need is a place to store memory for a little while, and then I can get rid of it.
-   (or just export it to a JSON file, if I want to keep it.)
- */
+/* This file contains the class that I use to interact with MongoDB.  */
 
 // mongoclient, to connect to mongodb
 var MongoClient = require("mongodb").MongoClient;
