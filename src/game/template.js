f9cb1619368c533987304c4aa196104e0f9e721b
zsarge <zackarywsargent@gmail.com>
2023-02-06 21:50:09 +0000

    fix INTERalliance stylization

--- src/game/template.js
+++ src/game/template.js
@@ -23,7 +23,7 @@ async function fillTemplate(content, id) {
 <head>
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <title>Interalliance Wiki Races</title>
+    <title>INTERalliance Wiki Races</title>
     <meta name="description"
         content="A website to host challenges where people try to get from one wikipedia page to another, as quick as possible.">
     <meta name="viewport" content="width=device-width, initial-scale=1">
